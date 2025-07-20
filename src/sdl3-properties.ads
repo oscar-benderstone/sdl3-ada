ef20128492148eaec78041719ada0e0cd57c133a
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-19 22:36:40 -0600

    Added more functions for properties

--- src/sdl3-properties.ads
+++ src/sdl3-properties.ads
@@ -13,9 +13,30 @@
 -- by integer IDS. They can be used as hashes:
 -- created, cleared, inserted, and destroyed.
 --
--- TODO: compare to Ada's existing hash tables. 
+-- TODO: compare to Ada's existing hash tables.
 
 package SDL3.Properties is
-   type Properties_ID is mod 2**32 with Convention => C;
+   type ID is mod 2**32 with Convention => C;
    type Property_Type is (Invalid, Pointer, String, Number, Float);
+
+   procedure Get_Global_Properties
+   with
+     Import        => True,
+     Convention    => C,
+     External_Name => "SDL_GetGlobalProperties";
+
+   -- TODO: maybe abstract this away?
+   function Create return ID
+   with
+     Import        => True,
+     Convention    => C,
+     External_Name => "SDL_CreateProperties";
+
+   function Copy (source : in out ID; dst : in out ID) return Boolean;
+
+   -- TODO: determine if this is compatible with
+   -- Ada's tasks!
+   function Lock (props : in out ID) return Boolean;
+
+   procedure Unlock (props : in out ID);
 end;
